35fb7c64f9f2ebced6b8a5e50331bd2b48524a0f
MD. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-03-04 22:24:20 +0600

    adding home page and write js code for login form to successfully switch one page to another

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-console.log("hello index");
+// login event
+document
+  .getElementById("btn-login")
+  .addEventListener("click", function (event) {
+    // to stop page reloading on form submit
+    event.preventDefault();
+
+    // get mobile number
+    const mobileNumberField = document.getElementById("input-mobile-number");
+    const mobileNumber = mobileNumberField.value;
+
+    // get pin number
+    const pinNumberField = document.getElementById("input-pin-number");
+    const pinNumber = pinNumberField.value;
+
+    // user mobile and pin validation
+    if (mobileNumber === "017" && pinNumber === "1234") {
+      // switch one page to another
+      window.location.href = "./home.html";
+
+      // switch one html page to another html page on a new tab
+      // window.open("./home.html");
+      // window.open("./home.html", "_blank");
+
+      // reset user input
+      mobileNumberField.value = "";
+      pinNumberField.value = "";
+    } else {
+      alert("Wrong Mobile Number or Pin Number. Please try again later!");
+    }
+  });
