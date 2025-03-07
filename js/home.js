35442b452af2f16c378eab4d1f7def4b554fd4c3
MD. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-03-07 19:45:23 +0600

    write some line of code for adding event hander on add money button

--- js/home.js
+++ js/home.js
@@ -2,3 +2,44 @@
 document.getElementById("btn-logout").addEventListener("click", function () {
   window.location.href = "./index.html";
 });
+
+// user action:: add money event handeler
+document
+  .getElementById("user-action-add-money")
+  .addEventListener("click", function (e) {
+    console.log("add");
+  });
+
+// button::add money event handeler
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    // stop form default loading on submit
+    event.preventDefault();
+
+    // read account number & pin number
+    const accountNumber = document.getElementById("input-account-number").value;
+    const accountPinNumber = document.getElementById("input-account-pin").value;
+
+    // read current amount & add money amount
+    const strCurrentBalance =
+      document.getElementById("current-balance").innerText;
+    const strAddMoney = document.getElementById("input-add-amount").value;
+
+    // validation
+    if (accountNumber === "1221" && accountPinNumber === "3456") {
+      const numCurrentBalance = parseFloat(strCurrentBalance);
+      const numAddMoney = parseFloat(strAddMoney);
+
+      const updatedBalance = numCurrentBalance + numAddMoney;
+
+      document.getElementById("current-balance").innerText = updatedBalance;
+
+      // reset input
+      document.getElementById("input-account-number").value = "";
+      document.getElementById("input-account-pin").value = "";
+      document.getElementById("input-add-amount").value = "";
+    } else {
+      alert("Wrong Account Number or Pin Number. Try again later");
+    }
+  });
