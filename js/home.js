1990023ee0fc856d8aa2f6b6c258ef89dc359351
MD. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-03-08 12:07:36 +0600

    update all element getting single line of code into some small function parts

--- js/home.js
+++ js/home.js
@@ -10,36 +10,98 @@ document
     console.log("add");
   });
 
-// button::add money event handeler
+/*
+-------------------common function to get Element value---------
+*/
+
+// get user input value
+function getInputFieldStrValue(fieldId) {
+  const fieldStrValue = document.getElementById(fieldId).value;
+
+  return fieldStrValue;
+}
+
+// get current balance
+function getCurrentBalance() {
+  const currentBalanceStrValue =
+    document.getElementById("current-balance").innerText;
+  const currentBalanceNumValue = parseFloat(currentBalanceStrValue);
+
+  return currentBalanceNumValue;
+}
+
+// get input amount
+function getInputAmount(inputAmountId) {
+  const inputAmountStrValue = document.getElementById(inputAmountId).value;
+  // const inputAmountNumValue = parseFloat(inputAmountStrValue);
+  const inputAmountNumValue = Number(inputAmountStrValue);
+
+  return inputAmountNumValue;
+}
+
+// set or update  current balance
+function updateCurrentBalance(updatedValue) {
+  document.getElementById("current-balance").innerText = updatedValue;
+}
+
+// reset input field value
+function resetFieldValue(inputId) {
+  document.getElementById(inputId).value = "";
+}
+
+// submit button::add money event handeler
 document
   .getElementById("btn-add-money")
   .addEventListener("click", function (event) {
     // stop form default loading on submit
     event.preventDefault();
 
-    // read account number & pin number
-    const accountNumber = document.getElementById("input-account-number").value;
-    const accountPinNumber = document.getElementById("input-account-pin").value;
-
-    // read current amount & add money amount
-    const strCurrentBalance =
-      document.getElementById("current-balance").innerText;
-    const strAddMoney = document.getElementById("input-add-amount").value;
+    // get account number & pin number
+    const accountNumber = getInputFieldStrValue("input-account-number");
+    const accountPinNumber = getInputFieldStrValue("input-account-pin");
 
     // validation
     if (accountNumber === "1221" && accountPinNumber === "3456") {
-      const numCurrentBalance = parseFloat(strCurrentBalance);
-      const numAddMoney = parseFloat(strAddMoney);
+      // get balance
+      const currentBalance = getCurrentBalance();
+      const addMoney = getInputAmount("input-add-amount");
 
-      const updatedBalance = numCurrentBalance + numAddMoney;
+      // upadate blance and set it
+      const updatedBalance = currentBalance + addMoney;
+      updateCurrentBalance(updatedBalance);
 
-      document.getElementById("current-balance").innerText = updatedBalance;
-
-      // reset input
-      document.getElementById("input-account-number").value = "";
-      document.getElementById("input-account-pin").value = "";
-      document.getElementById("input-add-amount").value = "";
+      // reset input field
+      resetFieldValue("input-account-number");
+      resetFieldValue("input-account-pin");
+      resetFieldValue("input-add-amount");
     } else {
       alert("Wrong Account Number or Pin Number. Try again later");
     }
   });
+
+// submit button:: cashout event handerler
+// document
+//   .getElementById("btn-cashout")
+//   .addEventListener("click", function (event) {
+//     // stop from reloading on submit
+//     event.preventDefault();
+
+//     // read account number and pin number element
+//     const agentNumber = document.getElementById("input-agent-number").value;
+//     const cashoutPin = document.getElementById("input-cashout-pin").value;
+
+//     // read balance element
+//     const strCurrentBalance =
+//       document.getElementById("current-balance").innerText;
+//     const strWithdrawMoney = document.getElementById(
+//       "input-cashout-amount"
+//     ).value;
+
+//     // validation
+//     if (agentNumber === "1221" && cashoutPin === "3456") {
+//       // const
+//     } else {
+//       alert("Wrong Agent Number or Pin Number. Try Again Later");
+//     }
+//     console.log(cashoutPin);
+//   });
